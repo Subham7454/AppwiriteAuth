b618fb5ac1b6e2bfdfb154cd9713483a5a8df005
Subham <shubhamrawat1843@gmail.com>
2024-09-28 11:30:50 +0530

    debig

--- src/appwrite/service.ts
+++ src/appwrite/service.ts
@@ -1,8 +1,6 @@
-//resusable service
-
-import { ID, Account, Client } from 'appwrite'
-import config from 'react-native-config'
-import Snackbar from 'react-native-snackbar'
+import { ID, Account, Client } from 'appwrite';
+import config from 'react-native-config';
+import Snackbar from 'react-native-snackbar';
 
 const appwriteClient = new Client();
 
@@ -10,92 +8,91 @@ const APPWRITE_ENDPOINT: string = config.APPWRITE_ENDPOINT!;
 const APPWRITE_PROJECT_ID: string = config.APPWRITE_PROJECT_ID!;
 
 type CreateUserAccount = {
-    email: string;
-    userName: string;
-    password: string;
- 
-}
+  email: string;
+  userName: string;
+  password: string;
+};
 
 type LoginUserAccount = {
-    email: string;
-    password: string;
-}
+  email: string;
+  password: string;
+};
 
-class AppwriteService{
+class AppwriteService {
+  account;
 
-    account;
-    
-    constructor(){   //for connecting to appwrite
-
+  constructor() {
+    // Initialize Appwrite client
     appwriteClient
-    .setEndpoint(APPWRITE_ENDPOINT)
-    .setProject(APPWRITE_PROJECT_ID)
-        
-    this.account=new Account(appwriteClient)
-    }
-    
-    //create a new record of user inside appwrite
-    
-    async CreateAccount = ({ email, password, userName }: CreateUserAccount)=>{
-     try {
-         const UserAccount= await this.account.create(
-             ID.unique(),
-             email,
-             password,
-             userName
-         )
-         if (UserAccount) {
-            //TODO:create login feature
-             return .this.LoginUser({ email, password })
-         }
-         else {
-             return UserAccount;
-         }
-     } catch (error) {
-         Snackbar.show({
-             text: String(error),
-             duration:Snackbar.LENGTH_LONG
-         })
-         console.log("error::CreateAccount"+error)
-     }
-        
-    }
-    
-    async LoginUser = ({ email, password }: LoginUserAccount)=>{
-    try {
-        return await this.account.createEmailPasswordSession('email@example.com', 'password');
+      .setEndpoint(APPWRITE_ENDPOINT)
+      .setProject(APPWRITE_PROJECT_ID);
 
-    } catch (error) {
-        Snackbar.show({
-            text: String(error),
-            duration: Snackbar.LENGTH_LONG
-        })
-        console.log("error::Login user"+error)
-    }
-    
-    }
-    
-    async getCurrentUser() {
+    this.account = new Account(appwriteClient);
+  }
+
+  // Create a new record for the user inside Appwrite
+  async createAccount({ email, password, userName }: CreateUserAccount) {
     try {
-       return await this.account.get();
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        userName
+      );
+
+      if (userAccount) {
+        // If account creation is successful, log the user in
+        return this.loginUser({ email, password });
+      } else {
+        return userAccount;
+      }
     } catch (error) {
-        Snackbar.show({
-            text: String(error),
-            duration: Snackbar.LENGTH_LONG
-        })
-        console.log("error:: getCurrentuser"+error)
+      Snackbar.show({
+        text: String(error),
+        duration: Snackbar.LENGTH_LONG,
+      });
+      console.log('error::CreateAccount ' + error);
     }
-    }
-    async logout() {
+  }
+
+  // Log in the user
+  async loginUser({ email, password }: LoginUserAccount) {
     try {
-       return await this.account.deleteSession('current');
+      return await this.account.createSession(email, password);
     } catch (error) {
-        Snackbar.show({
-            text: String(error),
-            duration: Snackbar.LENGTH_LONG
-        })
-        console.log("error:: getCurrentuser"+error)
+      Snackbar.show({
+        text: String(error),
+        duration: Snackbar.LENGTH_LONG,
+      });
+      console.log('error::Login user ' + error);
     }
+  }
+
+  // Get the current user
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      Snackbar.show({
+        text: String(error),
+        duration: Snackbar.LENGTH_LONG,
+      });
+      console.log('error::getCurrentUser ' + error);
     }
+  }
+
+  // Log out the user
+  async logout() {
+    try {
+      return await this.account.deleteSession('current');
+    } catch (error) {
+      Snackbar.show({
+        text: String(error),
+        duration: Snackbar.LENGTH_LONG,
+      });
+      console.log('error::logout ' + error);
+    }
+  }
 }
+
 export default AppwriteService;
